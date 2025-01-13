8ba44ef6af0b17312d077b6a5ce980eab69ddfad
Riccardo Gucciardo <riccardogucciardo003@gmail.com>
2025-01-13 16:57:52 +0100

    fine esercizio

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -14,7 +14,7 @@ const fourthTeacher = teachers.at(3);
 console.log(fourthTeacher)
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers.splice(6,0, "patrick")
+teachers.splice(4,0, "patrick")
 console.log(teachers)
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.slice(7);
@@ -34,7 +34,7 @@ console.log(teachers)
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = teachers[6];
+const lewisIndex = teachers.lastIndexOf("Lewis");
 console.log(lewisIndex)
 
 // 8. Verifica se l'array teachers è vuoto
